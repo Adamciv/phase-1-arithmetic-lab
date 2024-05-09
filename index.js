5d0282a2f41b41f45ca27fcac4fb4832ab259405
ahmedj <ahmedarifusa@gmail.com>
2024-05-09 14:04:19 +0300

    updated

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+const num1 = 6.2
+const num2 = 10
+ const multiply = num1*num2
+
+
+
+ const random = Math.floor(Math.random()) + 1;
+
+ const num3 = 16
+ const num4 = 6
+ const mod = num3%num4
+
+ const max = Math.max(2,3,20,4,7,14)
+
+
+
+ console.log(16/6)
